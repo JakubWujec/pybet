4cabb1e27840044461791e1fd321f8a3e2835443
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-12-19 20:51:00 +0100

    remove print from config

--- src/config.py
+++ src/config.py
@@ -2,10 +2,11 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
-print(f"FILE XDD {__file__}")
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
         'sqlite:///' + os.path.join(basedir, 'dev.db')
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle' : 280}
