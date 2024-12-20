3e3537ab96d04bccbe06485149c665090d4f753e
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-12-20 12:15:29 +0100

    flask-admin installed

--- init_db.py
+++ init_db.py
@@ -1,11 +1,10 @@
 from sqlalchemy import create_engine
-from src.pybet.schema import metadata  # Replace with actual schema import
-from src.config import Config  # Replace with actual config import
+from src.pybet.schema import metadata
+from src.config import Config 
 
 def initialize_database():
     engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, echo=True)
     metadata.create_all(engine)
-
 
 if __name__ == "__main__":
     initialize_database()
--- src/flasky/__init__.py
+++ src/flasky/__init__.py
@@ -1,9 +1,24 @@
 from flask import Flask, request, render_template, flash, redirect
+from src.pybet import schema
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+from src.config import Config 
+
 from src import config
 
 def create_app(config_class=config.Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    
+    engine = create_engine(Config.SQLALCHEMY_DATABASE_URI, echo=True)
+    schema.metadata.create_all(engine)
+    session = sessionmaker(bind=engine)()
+
+    admin = Admin(app, name='pybet')
+    admin.add_view(ModelView(schema.Match, session))
+    admin.add_view(ModelView(schema.User, session))
 
     from src.flasky.main import bp as main_bp
     app.register_blueprint(main_bp)
