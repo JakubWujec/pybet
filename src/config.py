b27d039624167d5f0c70084cc42773c542f206db
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-12-20 15:32:37 +0100

    add sessionfactory to config

--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
 import os 
 from dotenv import load_dotenv
+from sqlalchemy.orm.session import Session, sessionmaker
+from sqlalchemy import create_engine
+from src.pybet import schema
 
 basedir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
 load_dotenv(os.path.join(basedir, '.env'))
@@ -10,3 +13,13 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'dev.db')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_ENGINE_OPTIONS = {'pool_recycle' : 280}
+    
+def get_db_uri():
+    return Config.SQLALCHEMY_DATABASE_URI
+
+def get_session_factory():
+    return sessionmaker(bind=create_engine(get_db_uri()))
+
+def get_session():
+    return sessionmaker()
+
