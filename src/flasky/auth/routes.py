0a1faec37745dc5d6ab4ad18fd2de76c99b40736
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-12-20 16:20:32 +0100

    register view

--- src/flasky/auth/routes.py
+++ src/flasky/auth/routes.py
@@ -3,21 +3,29 @@ from flask_login import current_user, login_user
 import sqlalchemy as sa
 from src.pybet import schema
 from src.flasky.forms.login_form import LoginForm
+from src.flasky.forms.register_form import RegisterForm
 from flask import redirect, render_template, flash, url_for
-
+from src.config import get_session
 
 @bp.route('/login', methods=['GET', 'POST'])
-def login():
+def login_view():
     form = LoginForm()
-    # if current_user.is_authenticated:
-    #     return redirect(url_for('index'))
-    # form = LoginForm()
-    # if form.validate_on_submit():
-    #     user = db.session.scalar(
-    #         sa.select(schema.User).where(schema.User.username == form.username.data))
-    #     if user is None or not user.check_password(form.password.data):
-    #         flash('Invalid username or password')
-    #         return redirect(url_for('login'))
-    #     login_user(user, remember=form.remember_me.data)
-    #     return redirect(url_for('index'))
+    if form.validate_on_submit():
+        session = get_session()
+        user = session.scalar(
+            sa.select(schema.User).where(schema.User.username == form.username.data))
+        
+        if user is None or not user.check_password(form.password.data):
+            flash("Wrong credentials", category="error")
+            return redirect(url_for('auth.login'))
+        
+        login_user(user)
+        return redirect('/index')
     return render_template('login.html', title='Login', form=form)
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register_view():
+    form = RegisterForm()
+    
+    return render_template('register.html', title='Register', form=form)
--- src/flasky/forms/register_form.py
+++ src/flasky/forms/register_form.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import SubmitField, IntegerField, StringField, PasswordField
 from wtforms.validators import DataRequired
 
-class LoginForm(FlaskForm):
+class RegisterForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    submit = SubmitField('Register')
