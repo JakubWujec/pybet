6c771cc23078f235e6ba744327de80df0d44c354
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-12-20 21:01:24 +0100

    login required

--- src/flasky/main/routes.py
+++ src/flasky/main/routes.py
@@ -4,6 +4,7 @@ from src.pybet import message_bus, commands
 from flask import request, render_template, flash, redirect
 from src.flasky.forms.bet_form import BetForm
 from src.flasky.forms.match_form import MatchForm
+from flask_login import login_user, logout_user, current_user, login_required
 import datetime
 
 @bp.route("/api/bets", methods=["POST"])
@@ -58,11 +59,23 @@ def update_score(match_id):
     
     return "OK", 201
 
+
 @bp.route("/matches/<match_id>/bet", methods=["GET", "POST"])
+@login_required
 def make_a_bet_form(match_id):
     form = BetForm()
     
     if form.validate_on_submit():
+        uow = unit_of_work.SqlAlchemyUnitOfWork()
+        message_bus.handle(
+            commands.MakeBetCommand(
+                user_id=current_user.id,
+                match_id=match_id,
+                home_team_score=form.home_team_score.data,
+                away_team_score=form.away_team_score.data,
+            ),
+            uow
+        )
         flash('Submitted data {}, remember_me={}'.format(
             form.home_team_score.data, form.away_team_score.data))
         return redirect('/index')
@@ -102,15 +115,5 @@ def testme():
 @bp.route("/")
 @bp.route("/index")
 def index():
-    user = {'username': 'John'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    user = current_user
+    return render_template('index.html', title='Home', user=user)
