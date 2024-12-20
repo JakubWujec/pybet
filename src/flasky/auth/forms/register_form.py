c363680daf80c8d698a7e5f0e9fd4b40b6f667c9
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-12-20 18:13:04 +0100

    register form validates username

--- src/flasky/auth/forms/register_form.py
+++ src/flasky/auth/forms/register_form.py
@@ -1,8 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, IntegerField, StringField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from src.pybet import schema
+from src.config import get_session
+import sqlalchemy as sa
 
 class RegisterForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('Register')
+    
+    def validate_username(self, username):
+        session = get_session()
+        user = session.scalar(sa.select(schema.User).where(
+            schema.User.username == username.data))
+        if user is not None:
+            raise ValidationError('Please use a different username.')
