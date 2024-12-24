6ba4203aed1594baf8a6a5d2ac2a773c287596d8
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2024-12-24 11:02:44 +0100

    test mybetform and validators

--- src/flasky/mybet/forms.py
+++ src/flasky/mybet/forms.py
@@ -1,16 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import Form, FieldList, FormField, IntegerField, HiddenField, SubmitField
 from wtforms.widgets import HiddenInput
-from wtforms.validators import DataRequired, NumberRange
+from wtforms.validators import DataRequired, NumberRange, InputRequired, ValidationError
 
 class HiddenInteger(IntegerField):
     widget = HiddenInput()
 
+def non_negative_integer_required(form, field):
+    if field.data is None or field.data < 0:
+        raise ValidationError("Field must be non-negative integer.")
+
 # Subform for individual matches
 class MatchBetForm(Form):
     match_id = IntegerField(validators=[DataRequired()])
-    home_team_score = IntegerField("Home Score", validators=[DataRequired(), NumberRange(min=0)], default=0)
-    away_team_score = IntegerField("Away Score", validators=[DataRequired(), NumberRange(min=0)], default=0)
+    home_team_score = IntegerField("Home Score", validators=[non_negative_integer_required])
+    away_team_score = IntegerField("Away Score", validators=[non_negative_integer_required])
 
 # Main form to hold all matches
 class MatchBetListForm(FlaskForm):
