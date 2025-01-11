1730fb2bf24b6808ce7259cabc9a4d4a69a167c5
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-01-11 09:38:39 +0100

    redirect from point to bets when no round is active

--- src/flasky/main/routes.py
+++ src/flasky/main/routes.py
@@ -2,7 +2,7 @@ from src.flasky.main import bp
 from src.pybet import schema, unit_of_work
 from src.pybet import message_bus, commands
 from src.pybet import queries
-from flask import request, render_template, flash, redirect
+from flask import request, render_template, flash, redirect, url_for
 from flask_login import login_user, logout_user, current_user, login_required
 import datetime
 
@@ -12,6 +12,11 @@ def points():
     uow = unit_of_work.SqlAlchemyUnitOfWork()
     user_id = current_user.id
     active_gameround = queries.get_active_gameround_by_date(datetime.datetime.now(), uow)
+    
+    if active_gameround is None:
+        flash("There is no data to show yet!")
+        return redirect(url_for('my-bet.mybets_view'))
+    
     return redirect(f'/entry/{user_id}/rounds/{active_gameround}')
 
 @bp.route("/api/bets", methods=["POST"])
