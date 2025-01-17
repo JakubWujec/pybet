e50b79a63733cf8bf6d67d02d0c99ec968e0bee9
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-01-17 20:21:34 +0100

    Wrap update score script with uow

--- seeds/fpl_update_score.py
+++ seeds/fpl_update_score.py
@@ -12,27 +12,28 @@ def update_score_from_fpl(round: int):
     
     uow = unit_of_work.SqlAlchemyUnitOfWork()
         
-    for fixture in data:
-        if fixture['finished']:
-            home_team_fpl_id = fixture['team_h']
-            away_team_fpl_id = fixture['team_a']
-            home_team_code = get_team_code_by_fpl_id(home_team_fpl_id)
-            away_team_code = get_team_code_by_fpl_id(away_team_fpl_id)
-            home_team_id = queries.get_team_id_by_name(home_team_code, uow)
-            away_team_id = queries.get_team_id_by_name(away_team_code, uow)
-            home_team_score = fixture['team_h_score']
-            away_team_score = fixture['team_a_score']
-            match_id = queries.find_match_id(round, home_team_id, away_team_id, uow)
-            
-            if match_id:
-                message_bus.handle(
-                    commands.UpdateMatchScoreCommand(
-                        match_id=match_id,
-                        home_team_score=home_team_score,
-                        away_team_score=away_team_score
-                    ),
-                    uow
-                )
+    with uow:
+        for fixture in data:
+            if fixture['finished']:
+                home_team_fpl_id = fixture['team_h']
+                away_team_fpl_id = fixture['team_a']
+                home_team_code = get_team_code_by_fpl_id(home_team_fpl_id)
+                away_team_code = get_team_code_by_fpl_id(away_team_fpl_id)
+                home_team_id = queries.get_team_id_by_name(home_team_code, uow)
+                away_team_id = queries.get_team_id_by_name(away_team_code, uow)
+                home_team_score = fixture['team_h_score']
+                away_team_score = fixture['team_a_score']
+                match_id = queries.find_match_id(round, home_team_id, away_team_id, uow)
+                
+                if match_id:
+                    message_bus.handle(
+                        commands.UpdateMatchScoreCommand(
+                            match_id=match_id,
+                            home_team_score=home_team_score,
+                            away_team_score=away_team_score
+                        ),
+                        uow
+                    )
 
 
 
