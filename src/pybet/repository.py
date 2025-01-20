0cb63f83a14389fed77ef74e53888af2b7f732bc
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-01-20 15:40:51 +0100

    refactor repository

--- src/pybet/repository.py
+++ src/pybet/repository.py
@@ -1,15 +1,20 @@
 import abc
 from src.pybet import schema
-from typing import List
+from typing import List, Set
 
 class MatchRepository(abc.ABC):
-    @abc.abstractmethod
-    def add(self, match: schema.Match):
-        raise NotImplementedError
+    def __init__(self):
+        self.seen: Set[schema.Match] = set()
         
-    @abc.abstractmethod
+    def add(self, match: schema.Match):
+        self._add(match)
+        self.seen.add(match)
+        
     def get(self, match_id: int) -> schema.Match:
-        raise NotImplementedError 
+        match = self._get(match_id)
+        if match:
+            self.seen.add(match)
+        return match
     
     @abc.abstractmethod
     def list(self) -> List[schema.Match]:
@@ -18,19 +23,28 @@ class MatchRepository(abc.ABC):
     @abc.abstractmethod
     def get_gameround_matches(self, gameround: int):
         raise NotImplementedError
+    
+    @abc.abstractmethod
+    def _add(self, match: schema.Match):
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def _get(self, match_id) -> schema.Match:
+        raise NotImplementedError
 
-class FakeMatchRepository:
+class FakeMatchRepository(MatchRepository):
     def __init__(self):
+        super().__init__()
         self.matches = {}
         self._next_id = 1
         
-    def add(self, match: schema.Match):
+    def _add(self, match: schema.Match):
         if match.id is None:
             match.id = self._next_id
             self._next_id +=1 
         self.matches[match.id] = match
         
-    def get(self, match_id: int) -> schema.Match:
+    def _get(self, match_id: int) -> schema.Match:
         return self.matches[match_id]   
 
     def list(self):
@@ -45,16 +59,15 @@ class FakeMatchRepository:
         )
 
 
-class SqlMatchRepository:
+class SqlMatchRepository(MatchRepository):
     def __init__(self, session):
         super().__init__()
         self.session = session
     
-    def add(self, match: schema.Match):
+    def _add(self, match: schema.Match):
         self.session.add(match)
-        self.session.commit()
     
-    def get(self, match_id: int):
+    def _get(self, match_id: int):
         return self.session.query(schema.Match).filter_by(id=match_id).first()
     
     def get_gameround_matches(self, gameround: int):
