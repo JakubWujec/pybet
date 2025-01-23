ab9279d58ffb14d9222f361cc746d959e6fc1ce0
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-01-23 11:14:26 +0100

    pythonanwyhere task

---
+++ seeds/python_anywhere_task.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from seeds.fpl_update_score import update_score_from_fpl
+from seeds.make_bot_bets import make_bot_bets
+from pybet import unit_of_work
+from pybet.queries import get_active_gameround_by_date
+
+if __name__ == "__main__":    
+    uow = unit_of_work.SqlAlchemyUnitOfWork()
+    current_gameround = get_active_gameround_by_date(current_timestamp=datetime.now(), uow=uow)
+    print("CURRENT GAMEROUND: current_gameround")
+    update_score_from_fpl(current_gameround)
+    make_bot_bets(current_gameround + 1)
+        
+     
