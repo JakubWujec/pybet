4a524be6fa1af8dac10801d3e96d624c411e6b2c
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-03-20 20:52:09 +0100

    fpl_update_score - send command only if score differ

--- seeds/fpl_update_score.py
+++ seeds/fpl_update_score.py
@@ -2,36 +2,47 @@ from pybet import message_bus, commands, unit_of_work
 from pybet.score_provider import FPLScoreProvider
 import sys
 
-#>py -m seeds.fpl_update_score
+# >py -m seeds.fpl_update_score
+
 
 def update_score_from_fpl(round: int):
     score_provider = FPLScoreProvider()
     match_results = score_provider.get_match_results(round)
-    
-   
+
     uow = unit_of_work.SqlAlchemyUnitOfWork()
-    
+
     with uow:
         matches = uow.matches.get_gameround_matches(round)
-            
+
         for _match in matches:
-            found_result = next((result for result in match_results if result.home_team_short_name == _match.home_team.name and result.away_team_short_name == _match.away_team.name and result.finished == True), None)
+            found_result = next(
+                (
+                    result
+                    for result in match_results
+                    if result.home_team_short_name == _match.home_team.name
+                    and result.away_team_short_name == _match.away_team.name
+                    and result.finished == True
+                ),
+                None,
+            )
             if found_result:
-                message_bus.handle(
-                    commands.UpdateMatchScoreCommand(
-                        match_id=_match.id,
-                        home_team_score=found_result.home_team_score,
-                        away_team_score=found_result.away_team_score
-                    ),
-                    uow
-                )
+                if (
+                    found_result.home_team_score != _match.home_team_score
+                    or found_result.away_team_score != _match.away_team_score
+                ):
+                    message_bus.handle(
+                        commands.UpdateMatchScoreCommand(
+                            match_id=_match.id,
+                            home_team_score=found_result.home_team_score,
+                            away_team_score=found_result.away_team_score,
+                        ),
+                        uow,
+                    )
 
 
-
-if __name__ == "__main__":  
+if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Provide gameround")
     else:
-        p1 = sys.argv[1]  
+        p1 = sys.argv[1]
         update_score_from_fpl(int(p1))
-            
