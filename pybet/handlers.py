2063da6844b302312e55b1ec6d0736ff83b7f5b0
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-06-08 12:30:16 +0200

    Allow to fill bot bets after gameweek started

--- pybet/handlers.py
+++ pybet/handlers.py
@@ -1,60 +1,81 @@
-from pybet import schema, unit_of_work, commands, events, message_bus, services
-
 from datetime import datetime, timezone
+
+from pybet import commands, events, schema, services, unit_of_work
+
 
 class MatchAlreadyStarted(Exception):
     pass
 
+
 class MatchNotFound(Exception):
     pass
+
 
 def create_match(command: commands.CreateMatchCommand, uow: unit_of_work.UnitOfWork):
     with uow:
         match = schema.Match(
             home_team_id=command.home_team_id,
-            away_team_id = command.away_team_id,
-            gameround = command.gameround,
-            kickoff = command.kickoff
+            away_team_id=command.away_team_id,
+            gameround=command.gameround,
+            kickoff=command.kickoff,
         )
         uow.matches.add(
             match=match,
         )
         uow.commit()
 
-def make_bet(command: commands.MakeBetCommand, uow: unit_of_work.UnitOfWork):
+
+def make_bet(
+    command: commands.MakeBetCommand,
+    uow: unit_of_work.UnitOfWork,
+    skip_validation=False,
+):
     with uow:
-        match = uow.matches.get(command.match_id)    
-        
+        match = uow.matches.get(command.match_id)
+
         if match is None:
             raise MatchNotFound()
-        
-        if match.is_after_kickoff():
-            raise MatchAlreadyStarted(f"Now: {datetime.now(timezone.utc)}, kickoff: {match.kickoff}")
-        
+
+        if not skip_validation and match.is_after_kickoff():
+            raise MatchAlreadyStarted(
+                f"Now: {datetime.now(timezone.utc)}, kickoff: {match.kickoff}"
+            )
+
         bet = schema.Bet(
             user_id=command.user_id,
             home_team_score=command.home_team_score,
             away_team_score=command.away_team_score,
-            points = 0
-        )    
+            points=0,
+        )
         match.place_bet(bet)
         uow.commit()
-    
+
         return match.id
 
-def update_match_score(command: commands.UpdateMatchScoreCommand, uow: unit_of_work.UnitOfWork):
+
+def update_match_score(
+    command: commands.UpdateMatchScoreCommand, uow: unit_of_work.UnitOfWork
+):
     with uow:
-        match = uow.matches.get(command.match_id)  
+        match = uow.matches.get(command.match_id)
         match.update_score(
             home_team_score=command.home_team_score,
-            away_team_score=command.away_team_score
+            away_team_score=command.away_team_score,
         )
         uow.commit()
         return match.id
 
-def update_bet_points_for_match(event: events.MatchScoreUpdated, uow: unit_of_work.UnitOfWork):
+
+def update_bet_points_for_match(
+    event: events.MatchScoreUpdated, uow: unit_of_work.UnitOfWork
+):
     with uow:
         match = uow.matches.get(event.match_id)
         for bet in match.bets.values():
-            bet.points = services.calculate_points(bet.home_team_score, bet.away_team_score, match.home_team_score, match.away_team_score)            
+            bet.points = services.calculate_points(
+                bet.home_team_score,
+                bet.away_team_score,
+                match.home_team_score,
+                match.away_team_score,
+            )
         uow.commit()
--- seeds/make_bot_bets.py
+++ seeds/make_bot_bets.py
@@ -1,6 +1,8 @@
-import sys
 import random
-from pybet import unit_of_work, message_bus, commands, schema
+import sys
+
+from pybet import commands, handlers, schema, unit_of_work
+
 
 def make_bot_bets(round: int):
     uow = unit_of_work.SqlAlchemyUnitOfWork()
@@ -9,21 +11,23 @@ def make_bot_bets(round: int):
         bots = uow.session.query(schema.User).filter_by(role=schema.Role.BOT).all()
 
         for match in matches:
-            for bot in bots: 
-                message_bus.handle(
+            for bot in bots:
+                handlers.make_bet(
                     commands.MakeBetCommand(
                         user_id=bot.id,
                         match_id=match.id,
                         home_team_score=random.randint(0, 5),
-                        away_team_score=random.randint(0, 5)
-                    ), 
-                    uow
+                        away_team_score=random.randint(0, 5),
+                    ),
+                    uow,
+                    skip_validation=True,
                 )
 
-if __name__ == "__main__":  
+
+if __name__ == "__main__":
+    print("Run Seed: Make bot bets")
     if len(sys.argv) < 2:
         print("Provide gameround")
     else:
-        p1 = sys.argv[1]  
+        p1 = sys.argv[1]
         make_bot_bets(int(p1))
-            
