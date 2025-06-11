d1e02ba1e07beebdc879fcc9011d48637cf41dcc
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-06-11 16:49:31 +0200

    display form

--- flasky/mygamestage/views.py
+++ flasky/mygamestage/views.py
@@ -1,7 +1,7 @@
-from flask import render_template
+from flask import render_template, request
 from flask_login import current_user, login_required
 
-from flasky.mygamestage import bp
+from flasky.mygamestage import bp, forms
 from pybet import unit_of_work, queries
 
 
@@ -17,10 +17,25 @@ def mygamestage_view():
     matches = queries.mygamestage(
         user_id=current_user.id, gamestage_id=gamestage["id"], uow=uow
     )["matches"]
+    match_by_id = {match["id"]: match for match in matches}
+    form = forms.MatchBetListForm()
+
+    if request.method == "GET":
+        for match in matches:
+            bet = getattr(match, "bet", dict())
+            form.bets.append_entry(
+                {
+                    "match_id": int(match["id"]),
+                    "home_team_score": bet.get("home_team_score", 0),
+                    "away_team_score": bet.get("away_team_score", 0),
+                }
+            )
 
     return render_template(
         "mygamestage/mygamestage.html",
         gameround_name=gamestage["name"],
+        form=form,
+        match_by_id=match_by_id,
         current_user=current_user,
         enumerate=enumerate,
     )
