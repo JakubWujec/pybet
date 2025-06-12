b8aca20b0b81a558fec98ab101812764d4d5f9ea
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-06-12 13:43:56 +0200

    remove gameround from query

--- tests/integration/test_gamestage_repo.py
+++ tests/integration/test_gamestage_repo.py
@@ -1,11 +1,10 @@
+import pytest
+from sqlalchemy.sql import text
+
+from pybet import schema
 from pybet.repositories import (
-    GamestageRepository,
-    FakeGamestageRepository,
     SqlGamestageRepository,
 )
-from pybet import schema
-from sqlalchemy.sql import text
-import pytest
 
 
 @pytest.fixture
@@ -15,8 +14,8 @@ def setup_gamestage_and_match(session):
     session.execute(text("INSERT INTO teams (id, name) VALUES (1, 'ARS'), (2, 'CHE')"))
     session.execute(
         text(
-            "INSERT INTO matches (id, home_team_id, away_team_id, gameround, gamestage_id)"
-            " VALUES (1, 1, 2, 1, 1)"
+            "INSERT INTO matches (id, home_team_id, away_team_id, gamestage_id)"
+            " VALUES (1, 1, 2, 1)"
         )
     )
 
