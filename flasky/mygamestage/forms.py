e0f0c8d6d09c4f4d9074867c2b91951b7ee94fbf
JakubWujec <57007753+JakubWujec@users.noreply.github.com>
2025-06-17 17:06:47 +0200

    use wtfform components

--- flasky/mygamestage/forms.py
+++ flasky/mygamestage/forms.py
@@ -1,20 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import Form, FieldList, FormField, IntegerField, HiddenField, SubmitField
+from wtforms import FieldList, Form, FormField, IntegerField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
 from wtforms.widgets import HiddenInput
-from wtforms.validators import DataRequired, NumberRange, InputRequired, ValidationError
+
 
 class HiddenInteger(IntegerField):
     widget = HiddenInput()
+
 
 def non_negative_integer_required(form, field):
     if field.data is None or field.data < 0:
         raise ValidationError("Field must be non-negative integer.")
 
+
 # Subform for individual matches
 class MatchBetForm(Form):
-    match_id = IntegerField(validators=[DataRequired()])
-    home_team_score = IntegerField("Home Score", validators=[non_negative_integer_required])
-    away_team_score = IntegerField("Away Score", validators=[non_negative_integer_required])
+    match_id = HiddenInteger(validators=[DataRequired()])
+    home_team_score = IntegerField(
+        "Home Score", validators=[non_negative_integer_required]
+    )
+    away_team_score = IntegerField(
+        "Away Score", validators=[non_negative_integer_required]
+    )
+
 
 # Main form to hold all matches
 class MatchBetListForm(FlaskForm):
